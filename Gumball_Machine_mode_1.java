0d35bc90b2b2192e448dde8fb6cde75711087ec6
kshahzad86 <33069299+kshahzad86@users.noreply.github.com>
2018-04-16 16:02:39 -0700

    Create Gumball_Machine_mode_1.java

---
+++ Gumball_Machine_mode_1.java
@@ -0,0 +1,63 @@
+
+class Gumball_machine_mode1 
+{
+	private int num_gumballs;
+	private boolean has_quarter;
+	
+	
+	
+	public  Gumball_machine_mode1 (int size)
+	{
+	  this.num_gumballs = size;
+	  this.has_quarter = false;
+	}
+
+	
+	
+	public void insertQuarter(int coin)
+	{
+		if(coin == 25)
+		{
+			this.has_quarter = true;
+		}
+		else
+		{
+			this.has_quarter = false;
+		}
+	}
+	
+	
+	
+	public void turnCrank()
+	{
+		if (this.has_quarter)
+		{
+			
+			
+			if (this.num_gumballs > 0)
+			{
+				
+			  this.num_gumballs--;
+			  this.has_quarter = false;
+			  System.out.println(  "Thanks for your quarter , Gumball ejected!"  );
+			  
+			}
+			
+		
+		   else
+		   {
+			System.out.println(  "No More Gumballs!  sorry, can't return your quarter."  );
+		   }
+			
+			
+	   }
+	
+		
+	else
+	{
+		System.out.println(  "Please insert a quarter"  );
+	}
+	
+	}
+
+}
